c31ce1dd29d4b85409c9a430891a5dce8374adc7
ofaruk35 <erensarikaya66@gmail.com>
2021-05-08 13:26:21 +0300

    version2

--- src/gitproje/Gitproje.java
+++ src/gitproje/Gitproje.java
@@ -15,7 +15,7 @@ public class Gitproje {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("ömer faruk");
     }
     
 }
